2b0db1cd83c52cca260cb4a3ec16716095d6488d
sgeor <spyros.georgiakakis@gmail.com>
2020-01-12 14:52:50 +0200

    -

---
+++ mygym/precache-manifest.efd37e52fa8979982efede3c9edb32f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65670bc3a685d383aece2ec34a539388",
+    "url": "/mygym/index.html"
+  },
+  {
+    "revision": "81540d5e02c6de9d3a5b",
+    "url": "/mygym/static/js/2.ab1f6f30.chunk.js"
+  },
+  {
+    "revision": "6d562dc32c7687c7ab18",
+    "url": "/mygym/static/js/main.1110412d.chunk.js"
+  },
+  {
+    "revision": "de6b155058f3a2dda696",
+    "url": "/mygym/static/js/runtime-main.d918d9f8.js"
+  }
+]);
---
+++ mygym/precache-manifest.f2846a2fc1c8ff302b719db995031bf9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee8d9d403353f4be1e65eecbf77fd19",
+    "url": "/mygym/index.html"
+  },
+  {
+    "revision": "d7cf0eea8ff019cd1e89",
+    "url": "/mygym/static/js/2.05d945f1.chunk.js"
+  },
+  {
+    "revision": "fc0ba57cc9b535d775f5",
+    "url": "/mygym/static/js/main.0ac15710.chunk.js"
+  },
+  {
+    "revision": "de6b155058f3a2dda696",
+    "url": "/mygym/static/js/runtime-main.d918d9f8.js"
+  }
+]);
