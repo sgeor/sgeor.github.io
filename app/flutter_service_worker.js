814c5a82760d04a5f21eb5476a785d7b57eed27a
sgeor <spyros.georgiakakis@gmail.com>
2020-10-11 11:11:06 +0300

    -

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -13,12 +13,12 @@ const RESOURCES = {
 "assets/NOTICES": "f7e7cc2f25e25728b055972971a9ca9b",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "04655ea22b8768a8941d8b63060e976c",
-"/": "04655ea22b8768a8941d8b63060e976c",
+"icons/Icon-192.png": "13351d9df01c14e1bf3a7f339a8e52e8",
+"icons/Icon-512.png": "6b52164cb7a3a46196db24ce52d0b52a",
+"index.html": "8837009129a70fd0af97db84aafbc499",
+"/": "8837009129a70fd0af97db84aafbc499",
 "main.dart.js": "2ba9806a3101e3b903ca329d13b3ffce",
-"manifest.json": "2e080bf827eba42635ddb60efc4fb603"
+"manifest.json": "d090d9b6bee92c30a1ea73b61c281746"
 };
 
 // The application shell files that are downloaded before a service worker can
