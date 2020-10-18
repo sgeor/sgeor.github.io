f816dfe42ef3f1cde60b45552590c94a2c8bc8f3
sgeor <spyros.georgiakakis@gmail.com>
2020-10-18 15:00:26 +0300

    -

---
+++ app/precache-manifest.8409878fb5923b0c798d6b17dd238b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb03be7a342c57363abc4d1e1db06943",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "8b409e9da60b2fcba68e",
+    "url": "/app/static/css/main.8e41afe8.chunk.css"
+  },
+  {
+    "revision": "285fc59c75a69fd545f2",
+    "url": "/app/static/js/2.fcfee533.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/app/static/js/2.fcfee533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b409e9da60b2fcba68e",
+    "url": "/app/static/js/main.ddd5dbbc.chunk.js"
+  },
+  {
+    "revision": "37c52f494db0575d038c",
+    "url": "/app/static/js/runtime-main.96325625.js"
+  }
+]);
